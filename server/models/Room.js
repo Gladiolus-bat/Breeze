b52db38a3d5537daeb03d65a36623a638a0e3259
Sabrina Pradhan <pradhansabrina32@gmail.com>
2026-08-16 10:45:36 +0545

    fix: correct typo in pricePerNight field in Room schema

--- server/models/Room.js
+++ server/models/Room.js
@@ -11,7 +11,7 @@ const roomSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    pricePerBight: {
+    pricePerNight: {
       type: Number,
       required: true,
     },
