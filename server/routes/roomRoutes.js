71186edba4dbaf2043fb084c6bb8bbc4058446a1
Sabrina Pradhan <pradhansabrina32@gmail.com>
2026-08-29 20:25:00 +0545

    feat: add updateRoom functionality to roomRoutes and user profile update and password change routes

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import protect, {restrictTo} from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
-import {createRoom, getRooms, getOwnerRooms, toggleRoomAvailability} from "../controllers/roomController.js";
+import {createRoom, getRooms, getOwnerRooms, updateRoom, toggleRoomAvailability} from "../controllers/roomController.js";
 
 const roomRouter = express.Router();
 
 roomRouter.post("/", protect, restrictTo("admin"), upload.array("images", 4), createRoom);
 roomRouter.get("/", getRooms);
 roomRouter.get("/owner", protect, restrictTo("admin"), getOwnerRooms);
+roomRouter.put("/:roomId", protect, restrictTo("admin"), upload.array("images", 4), updateRoom);
 roomRouter.post("/toggle-availability", protect, restrictTo("admin"), toggleRoomAvailability);
 
 export default roomRouter;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
+import upload from "../middleware/upload.js";
 import createUser from "../controllers/createUser.js";
 import loginUser from "../controllers/loginUser.js";
 import {
   getUserData,
   storeRecentSearchedCities,
+  updateProfile,
+  changePassword,
 } from "../controllers/userController.js";
 
 const userRouter = express.Router();
@@ -13,5 +16,7 @@ userRouter.post("/login", loginUser);
 userRouter.post("/register", createUser);
 userRouter.get("/", protect, getUserData);
 userRouter.post("/store-recent-search", protect, storeRecentSearchedCities);
+userRouter.put("/profile", protect, upload.single("image"), updateProfile);
+userRouter.put("/change-password", protect, changePassword);
 
 export default userRouter;
